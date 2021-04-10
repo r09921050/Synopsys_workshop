502f326e53f2d90828098e1c6323a77bfff437be
Waxpple <a0910618112@gmail.com>
2021-04-10 14:24:36 +0800

    Add files via upload

--- Lab6_AI_eqrthquake_camera_Template/src/led_toggle.c
+++ Lab6_AI_eqrthquake_camera_Template/src/led_toggle.c
@@ -1,12 +1,1 @@
- void led_toggle(int 100msec_cnt){
-    switch(100msec_cnt % 2)
-    {
-      case 0:
-        hal_gpio_set(&hal_led_g, GPIO_PIN_SET);
-        break;    
-      case 1:
-        hal_gpio_set(&hal_led_g, GPIO_PIN_RESET);
-        break;
-    }
- }
- 
+//NOP

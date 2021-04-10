c92448a42b450329215f430dda6544129d2c61de
Waxpple <a0910618112@gmail.com>
2021-04-10 14:50:10 +0800

    Add files via upload

--- Lab6_AI_eqrthquake_camera_Template/src/synopsys_wei_gpio.cc
+++ Lab6_AI_eqrthquake_camera_Template/src/synopsys_wei_gpio.cc
@@ -1,5 +1,8 @@
 #include "synopsys_wei_gpio.h"
 
+	HX_DRV_GPIO_E gpio_pin;                 /**< GPIO pin, assigned by user */
+    uint8_t gpio_data;                      /**< GPIO data. Assigned by user when as output direction. Assigned by Himax driver when as input direction */
+    HX_DRV_GPIO_DIRCTION_E gpio_direction;  /**< GPIO direction, assigned by user */
 
 HAL_RETURN hal_gpio_init(hx_drv_gpio_config_t * hal_gpio, HX_DRV_GPIO_E hal_gpio_pin, HX_DRV_GPIO_DIRCTION_E hal_gpio_dir, GPIO_PIN_DATA hal_gpio_data)
 {
@@ -27,7 +30,7 @@ HAL_RETURN hal_gpio_get(hx_drv_gpio_config_t * hal_gpio, GPIO_PIN_DATA * gpio_pi
       return HAL_ERROR;
   else
   {
-    *gpio_pin_data = (GPIO_PIN_DATA (hal_gpio->gpio_data));
+    *gpio_pin_data = hal_gpio->gpio_data;
     return HAL_OK;
   }
 }
